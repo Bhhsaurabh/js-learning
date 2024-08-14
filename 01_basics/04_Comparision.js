aa1f4c6e1f0f399bf18120e946fe77886a3c1e7f
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-14 21:22:08 +0530

    memory explained

--- 01_basics/04_Comparision.js
+++ 01_basics/04_Comparision.js
@@ -5,5 +5,13 @@
 
 // === is a strict check
 
-console.log("2" == 2)
-console.log("2" === 2)
+// console.log("2" == 2)
+// console.log("2" === 2)
+
+// symbol
+
+let numSym = Symbol('123')
+let numSym2 = Symbol('123')
+console.log(numSym === numSym2);
+let bigNum = 2321343214128n;
+console.log(bigNum);
---
+++ 01_basics/05_memory.js
@@ -0,0 +1,24 @@
+// * all the primitive data type have stack memory
+
+let myName = "Saurabh"
+let myName2 = myName
+myName2 = "golu"
+console.log(myName)
+console.log(myName2)
+
+// original value do not got change
+
+
+// * all the non primitive data type have heap memory
+
+let perOne = {
+    id:1,
+    mail : "bhhsaurabh@gamil.com",
+    name : "saurabh"
+}
+let perTwo = perOne
+perTwo.mail = "golu@gmail.com"
+console.log(perOne.mail);
+console.log(perTwo.mail)
+
+// here the original value got changed

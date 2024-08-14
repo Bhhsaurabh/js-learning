282bf0b532be1ff76f968c217d5a5c2315a04f78
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-14 16:02:18 +0530

    conversions done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+// num to stirng conversion
+let someNumber = 33
+let strNum = String(someNumber)
+//console.log(typeof strNum)
+// num/str to boolean converison
+
+let num = "saur" // at 1 true/ at 0 false / at "" false / at "saur" true
+let boolNum = Boolean(num)
+//console.log(boolNum)
+
+// ********************** Operations ****************
+console.log("1" + 2+2) // at ("1" + (2+2)) is 14 and at ("1" + 2+ 2) is 122
+console.log(+true)
+console.log(+"")

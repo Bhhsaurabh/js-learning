df4430ab641d8300ba14d5a685cc36273b43f4c8
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-14 22:27:15 +0530

    Strings explained

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,37 @@
+let name = "saurabh-kumar-singh"
+let numCount = 50
+
+//to concatenate two string old method
+//console.log(name + numCount + "value");
+
+// modern method 
+//console.log(`My name is ${name} my count is ${numCount}`)
+let newName = new String("golu")
+// console.log(name.__proto__)
+// console.log(newName.length);
+
+// method in strings examples
+
+// console.log(newName.toUpperCase());
+// console.log(newName.charAt(2))
+// console.log(newName.indexOf('o'))
+// let subString = name.substring(0,4)
+// let sliceString = name.slice(-7,2)
+// console.log(subString);
+// console.log(sliceString);
+
+// to remove spaces
+
+// let newStr = "   Saurabh     "
+// console.log(newStr);
+// console.log(newStr.trim());
+
+// to replace anyting example
+const url = "https://www.saurabh-Groups.com/saurabh%20md102"
+// console.log(url)
+// console.log(url.replace('%20','-'))
+// console.log(url.includes('saurabh'))
+
+// convert string to array
+
+console.log(name.split('-'))

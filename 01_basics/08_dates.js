7de312d6978a5893c3e9b7d7a265f9ad43f341e4
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-18 18:05:36 +0000

    dates explained

--- 01_basics/08_dates.js
+++ 01_basics/08_dates.js
@@ -7,4 +7,21 @@ let date = new Date() // it will give current date
 
 // create a new date object
 let newDate = new Date(2024,7,18)
-console.log(newDate.toDateString())
+// console.log(newDate.toDateString())
+
+//TODO: compare dates with different date objects
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp) // dates are in milliseconds
+//console.log(newDate.getTime()) // dates are in milliseconds
+
+// convert milliseconds to seconds
+
+//console.log(Math.floor(Date.now()/1000)) // dates are in seconds
+
+console.log(date.getDay())
+console.log(date.getMonth() + 1)
+
+// you can also customize the localString property
+
+console.log(newDate.toLocaleString('default',{weekday:"long"}));

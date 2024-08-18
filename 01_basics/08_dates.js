be75b066cf7afe242a74807e1ecafee0db6bbf49
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-18 17:38:26 +0000

    dates explained

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,10 @@
+// declare dates
+let date = new Date() // it will give current date
+// console.log(date)
+// console.log(date.toDateString())
+// console.log(date.toString())
+// console.log(date.toLocaleString())
+
+// create a new date object
+let newDate = new Date(2024,7,18)
+console.log(newDate.toDateString())

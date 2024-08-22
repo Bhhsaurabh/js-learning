26343e852724c1e354cb5ec7a09181dd699fffe0
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-22 16:13:38 +0000

    IIfe explained

---
+++ 03_basics/Iife.js
@@ -0,0 +1,25 @@
+// Intermediately invoked function exchange
+(function Twosum(){
+    console.log(5+2)
+})(); // here iife is called
+
+// TODO: write using arrow functions
+
+// ( (username) => {
+//     console.log(`Hello ${username}`)
+// })("Saurabh")
+
+// how the code executes
+
+const var1 = 10
+const var2 = 20
+
+// function Tsum(var1 , var2){
+//     const total_sum = var1 + var2
+//     return total_sum 
+
+// }
+// let result1 = Tsum(var1,var2)
+// let result2 = Tsum(8,9)
+
+

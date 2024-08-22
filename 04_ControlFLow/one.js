b684f41f9961a934c9d56486000da525aa5f9943
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-22 17:31:10 +0000

    if else explained

---
+++ 04_ControlFLow/one.js
@@ -0,0 +1,11 @@
+if(true){
+    console.log("hello world!");
+}else if(true){
+
+}else{
+
+}
+//or 
+if(true) console.log("Hello saurbah"), // you can use multiple lines using , comma with in if statement only
+console.log("hello saurabh 2");
+

2728768298c8f17ef1565310c9299a881ae85dbe
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-23 15:52:47 +0000

    for loop explained

---
+++ 05_Iterations/one.js
@@ -0,0 +1,21 @@
+// for 
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element)
+    
+// }
+// break 
+for (let index = 1; index < 10; index++) {
+    if(index === 5)
+        break; // loop got break after 4 
+    console.log(index)
+    
+}
+// continue 
+
+for (let index = 1; index < 10; index++) {
+    if(index === 5)
+        continue // loop ignores/skips element and proceed to next at one time 
+    console.log(index)
+    
+}

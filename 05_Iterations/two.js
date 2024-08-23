6c130d67829ae6eb7080768f69f3bcf210732efd
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-23 16:05:38 +0000

    while / do-while loop explained

---
+++ 05_Iterations/two.js
@@ -0,0 +1,18 @@
+// while loop
+let i =11
+// while(i<=10 ){
+//     console.log(i)
+//     i++
+// }
+// const arr = [1,2,3,4,5,6,7,8]
+// while(i<= arr.length ){
+//     console.log(arr[i])
+//     i++
+// }
+
+// do-while loop
+// do while loop first does its work then it checks for conditions
+do{
+    console.log(`the value is ${i}`)
+    i++
+}while(i<=10)
